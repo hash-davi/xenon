7bcbbaf763d26d2c5e6c379f8ee4e198b7a277c8
daviicastro <davi.castro1319@gmail.com>
2026-09-06 16:30:21 -0300

    started prototyping components

--- MainScreen.lua
+++ MainScreen.lua
@@ -1,7 +1,32 @@
 MainScreen = Class({})
 
+require("Header")
+require("Collection")
+
+function MainScreen:load()
+	self.x = 0
+	self.y = 0
+	self.width = WINDOW_WIDTH
+	self.height = WINDOW_HEIGHT
+
+	Header:load()
+	Collection:load({ width = self.width })
+end
+
 function MainScreen:render()
 	love.graphics.clear(0, 0, 0, 0)
-	love.graphics.setColor(1, 1, 1, 1)
-	love.graphics.printf("Xenon", WINDOW_WIDTH / 4, 200, WINDOW_WIDTH, "center")
+
+	MainScreen:debug()
+
+	Header:render()
+
+	Collection:render()
+end
+
+function MainScreen:debug()
+	love.graphics.setColor(0, 1, 0, 1)
+	love.graphics.line(0, 0, 0, self.height)
+	love.graphics.line(0, 0, self.width, 0)
+	love.graphics.line(self.width, 0, self.width, self.height)
+	love.graphics.line(self.width, self.height, 0, self.height)
 end
--- main.lua
+++ main.lua
@@ -1,8 +1,8 @@
 Class = require("libs.class")
 require("MainScreen")
 
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
+WINDOW_WIDTH = 1920
+WINDOW_HEIGHT = 1080
 
 function love.keypressed(key)
 	if key == "escape" then
@@ -10,14 +10,22 @@ function love.keypressed(key)
 	end
 end
 
+--function love.resize(w, h) end
+
 function love.load()
 	love.window.setTitle("Xenon")
 
 	love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
-		resizable = true,
+		resizable = false,
 		fullscreen = false,
 		vsync = true,
 	})
+
+	Fonts = {
+		["regular"] = love.graphics.newFont("fonts/JetBrainsMono-Regular.ttf", 32),
+	}
+
+	MainScreen:load()
 end
 
 function love.update(dt) end
